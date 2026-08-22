5b70f4e5073d25c0cff8f5d173a67dff74cb330f
rudrakshupadhyay <218912408+rudrakshupadhyay@user.noreply.github.com>
2026-08-22 15:29:10 +0530

    connected registerUI to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
--- reader-frontend/src/pages/register/register.jsx
+++ reader-frontend/src/pages/register/register.jsx
@@ -1,30 +1,107 @@
 import style from "./Register.module.css";
 import { Link } from "react-router";
+import { useState } from "react";
+
 function RegisterPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState([]);
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (!response.ok) {
+        console.log("Registration failed");
+        setError(data.errors || ["Registration failed"]);
+        return;
+      }
+
+      console.log("Registration successful");
+    } catch (error) {
+      setError([
+        "An error occurred during registration. Please try again later.",
+      ]);
+      console.error("Error:", error);
+    }
+  }
   return (
     <div className={style.registerPage}>
+      {error && (
+        <div className={style.error}>
+          <ul className={style.errorList}>
+            {error.map((err, index) => (
+              <li key={index}>{err.msg}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <div className={style.registerForm}>
         <div className={style.registerHeading}>
           <h1>Register</h1>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name" className={style.formLabel}>
               Name:
             </label>
-            <input type="text" id="name" className={style.formInput} />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className={style.formInput}
+              value={formData.name}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
             <label htmlFor="username" className={style.formLabel}>
               Username:
             </label>
-            <input type="text" id="username" className={style.formInput} />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className={style.formInput}
+              value={formData.username}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <label htmlFor="pass" className={style.formLabel}>
+            <label htmlFor="password" className={style.formLabel}>
               Password:
             </label>
-            <input type="password" id="pass" className={style.formInput} />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className={style.formInput}
+              value={formData.password}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
             Already have an account?
@@ -33,7 +110,9 @@ function RegisterPage() {
               Login here.
             </Link>
           </div>
-          <button className={style.registerButton}>Register</button>
+          <button type="submit" className={style.registerButton}>
+            Register
+          </button>
         </form>
       </div>
     </div>
